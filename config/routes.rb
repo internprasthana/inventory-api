8327265e68b31549028900bc1477dccfd7a87ee1
vamshikrishnaprasthana <vamshikrishna.pandi@parasthana.com>
2022-08-18 13:05:54 +0530

    User_login

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :users, param: :_username
     post '/auth/login', to: 'authentication#login'
-    resources :employees
-    resources :devices
-
-    resources :devices
-  resources :employees,except: [:new] do
-    member do
-      get 'devices'
-      #post 'chargers'
-    end
-  end
-  get 'signup',to: 'employees#new'
-  get 'login',to: 'sessions#new'
-  post 'login',to: 'sessions#create'
-  delete 'logout',to: 'sessions#destroy'
-
-
-
 end
