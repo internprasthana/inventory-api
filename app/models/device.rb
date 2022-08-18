c564447303361a7ad2d6c31642cd555bd9a20359
vamshikrishnaprasthana <108066269+vamshikrishnaprasthana@users.noreply.github.com>
2022-08-18 13:10:05 +0530

    Delete device.rb

--- app/models/device.rb
+++
@@ -1,9 +0,0 @@
-class Device < ApplicationRecord
-# :employee
-  # validates :name, presence: true
-  validates :service_tag, presence:true
-  has_one :employee
-
- validates :name, presence: true,length: { minimum: 3 }
- validates :device_type, presence: true
-end
