7bf84a40a618c5a272f9d122b0af8e527970ec30
dinakartejat <dinakarteja.tanniru@prasthana.com>
2022-08-19 12:15:49 +0530

    search on device and employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,4 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: [:show, :update, :destroy]
-
-  def index
-   @employees = Employee.all
-   render json: @employees
-  end
 
   def search
     @query = params[:query]
@@ -13,44 +7,5 @@ class EmployeesController < ApplicationController
       render json: @employees
     end
   end
-
-  def new
-    @employee = Employee.new
-    render json: @employee
-  end
-  def show
-    @employee = Employee.find(params[:id])
-    render json: @employee
-  end
-
-  def create
-    @employee = Employee.new(employee_params)
-
-    if @employee.save
-      render json: @employee, status: :created, location: @employee
-    else
-      render json: @employee.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @employee.update(employee_params)
-      render json: @employee
-    else
-      render json: @employee.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @employee.destroy
-  end
-
-  private
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
-
-    def employee_params
-      params.require(:employee).permit(:name,:email,:password,:reset_password,:emp_id,:role,:phone_number,:employee_id)
-    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,5 @@
 Rails.application.routes.draw do
-
-  put '/passwordsresets',to: 'passwordsresets#update'
-  
-
 get    '/devices/search',to: 'devices#search'
 get   '/employees/search',to: 'employees#search'
-
-
-  resources :users
-  post '/auth/login', to: 'authenications#login'
-
-     resources :devices
-      get    'devices/index', to: 'devices#index'
-       patch '/devices/',to: 'devices#update'
-        get '/devices/search',to: 'devices#search'
-
-
-
- resources :employees
- resources :passwordsresets
-get 'signup',to: 'employees#new'
-get 'login',to: 'sessions#new'
-post 'login',to: 'sessions#create'
-delete 'logout',to: 'sessions#destroy'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
