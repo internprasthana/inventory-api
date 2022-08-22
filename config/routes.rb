5247f360138bb71fb91fa741cbc8b2b1da276098
vamshikrishnaprasthana <vamshikrishna.pandi@parasthana.com>
2022-08-22 11:40:32 +0530

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
-  post '/auth/login', to: 'authentication#login'
+  post '/auth/login', to: 'authentications#login'
   resources :employees
   resources :devices
 end
